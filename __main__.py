de6de479f8b986eb8c33bad34f7c8902320c71cc
sabrynico21 <sabry.nico21@gmail.com>
2024-02-10 12:19:07 +0100

    insert train and test modalities

--- __main__.py
+++ __main__.py
@@ -2,17 +2,30 @@ import logging
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import argparse
 from transformers import BertTokenizer
 from torch.utils.data import DataLoader
 from .utils import read_json_file, calculate_class_weights, get_predicted_labels, get_performances
 from .entities import TextWithLabelsContainer
 from .constants import aspect_mapping, opinion_mapping
-from .model import AspectClassificationModel
+from .model import ClassificationModel
 torch.manual_seed(42)
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Train or test a model')
+    parser.add_argument('--mode', choices=['train', 'test'], help='Specify whether to train or test the model')
+    parser.add_argument('--model', help='Path to the model (required for the test mode)')
+    args = parser.parse_args()
+
     dataset = {}
-    for set in ['train', 'val','test']:
+
+    if args.mode == 'train':
+        sets = ['train', 'val']
+    else:
+        sets = ['train','test']
+
+    for set in sets:
         file_path =f'C:/Users/sabry/Downloads/rest16_quad_{set}.tsv.jsonl'
         dicts_data = read_json_file(file_path)
         dataset[set] = TextWithLabelsContainer([entry['text'] for entry in dicts_data])
@@ -31,47 +44,48 @@ if __name__ == "__main__":
             dataset[set].set_attention_mask(sentences_attention_mask[i])
             dataset[set].compute_one_hot_format(i, num_aspects, num_opinions)
 
-    model = AspectClassificationModel(bert_model_name='bert-base-uncased', num_aspects=num_aspects, num_opinions=num_opinions)
-    for param in model.bert.parameters():
-        param.requires_grad = False
-    
-    train_data = [(dataset["train"].get_tokens(pos),
-                  dataset["train"].get_attention_mask(pos),
-                  dataset["train"].get_aspect_label(pos),
-                  dataset["train"].get_opinion_label(pos)) for pos in range(dataset["train"].len)]
-        
-    val_data = [(dataset["val"].get_tokens(pos),
-                  dataset["val"].get_attention_mask(pos),
-                  dataset["val"].get_aspect_label(pos),
-                  dataset["val"].get_opinion_label(pos)) for pos in range(dataset["val"].len)]
-        
-    train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size=16, shuffle=True)
-    
+    model = ClassificationModel(bert_model_name='bert-base-uncased', num_aspects=num_aspects, num_opinions=num_opinions)
     aspect_weights = calculate_class_weights([torch.argmax(dataset["train"].get_aspect_label(pos), dim=1).long() for pos in range(dataset["train"].len)])
     opinion_weights = calculate_class_weights([torch.argmax(dataset["train"].get_opinion_label(pos), dim=1).long() for pos in range(dataset["train"].len)])
     
     criterion_aspect = nn.CrossEntropyLoss(weight=aspect_weights)
     criterion_opinion = nn.CrossEntropyLoss(weight=opinion_weights)
-
-    optimizer_aspect = optim.Adam(model.fc1.parameters(), lr=0.001)
-    optimizer_opinion = optim.Adam(model.fc2.parameters(), lr=0.001)
-    num_epochs = 10
-    logging.basicConfig(filename='output.log', level=logging.INFO, filemode='a') 
-    #model.train_and_save(num_epochs, train_loader, val_loader, criterion_aspect, criterion_opinion, optimizer_aspect, optimizer_opinion, num_aspects, num_opinions, logging)
-
-    test_data = [(dataset["test"].get_tokens(pos),
-                  dataset["test"].get_attention_mask(pos),
-                  dataset["test"].get_aspect_label(pos),
-                  dataset["test"].get_opinion_label(pos)) for pos in range(dataset["test"].len)]
-
-    test_loader = DataLoader(test_data, batch_size=dataset["test"].len, shuffle=False)
     
-    predictions = model.predict(num_aspects, num_opinions, criterion_aspect, criterion_opinion, test_loader)
-    
-    text_tokens_values = [item[0] for item in test_data]
-    texts = dataset["test"].get_texts()
-    label_predictions = get_predicted_labels(predictions, tokenizer, text_tokens_values, texts)
+    if args.mode == 'train':
+        for param in model.bert.parameters():
+            param.requires_grad = False
+        
+        train_data = [(dataset["train"].get_tokens(pos),
+                    dataset["train"].get_attention_mask(pos),
+                    dataset["train"].get_aspect_label(pos),
+                    dataset["train"].get_opinion_label(pos)) for pos in range(dataset["train"].len)]
+            
+        val_data = [(dataset["val"].get_tokens(pos),
+                    dataset["val"].get_attention_mask(pos),
+                    dataset["val"].get_aspect_label(pos),
+                    dataset["val"].get_opinion_label(pos)) for pos in range(dataset["val"].len)]
+            
+        train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
+        val_loader = DataLoader(val_data, batch_size=16, shuffle=True)
 
-    ground_truth = [ dataset["test"].get_text_labels(pos) for pos in range(dataset["test"].len)]  
-    get_performances(ground_truth, label_predictions)
+        optimizer_aspect = optim.Adam(model.fc1.parameters(), lr=0.001)
+        optimizer_opinion = optim.Adam(model.fc2.parameters(), lr=0.001)
+
+        num_epochs = 10
+        logging.basicConfig(filename='classification_model_metrics.log', level=logging.INFO, filemode='a') 
+        model.train_and_save(num_epochs, train_loader, val_loader, criterion_aspect, criterion_opinion, optimizer_aspect, optimizer_opinion, num_aspects, num_opinions, logging)   
+    else:
+        test_data = [(dataset["test"].get_tokens(pos),
+                    dataset["test"].get_attention_mask(pos),
+                    dataset["test"].get_aspect_label(pos),
+                    dataset["test"].get_opinion_label(pos)) for pos in range(dataset["test"].len)]
+        test_loader = DataLoader(test_data, batch_size=dataset["test"].len, shuffle=False)
+        #model_path = 'NLP_epoch_{}.pth'.format(3) 
+        predictions = model.predict(num_aspects, num_opinions, criterion_aspect, criterion_opinion, test_loader, args.model)
+        
+        text_tokens_values = [item[0] for item in test_data]
+        texts = dataset["test"].get_texts()
+        label_predictions = get_predicted_labels(predictions, tokenizer, text_tokens_values, texts)
+
+        ground_truth = [ dataset["test"].get_text_labels(pos) for pos in range(dataset["test"].len)]  
+        get_performances(ground_truth, label_predictions)
